66137efe947760b7c0a43feb691b5cf91602ad05
francis-mbithi <frankmbithifm@gmail.com>
2020-02-02 22:31:40 +0300

    add splash

---
+++ app/src/main/java/com/moringaschool/garbage/SplashActivity.java
@@ -0,0 +1,59 @@
+package com.moringaschool.garbage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+//    Handler handler;
+//    ImageView imageView;
+    Animation anim;
+    ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+//        handler=new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        },3000);
+
+        imageView=(ImageView)findViewById(R.id.logo_id); // Declare an imageView to show the animation.
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in); // Create the animation.
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();                // HomeActivity.class is the activity to go after showing the splash screen.
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        imageView.startAnimation(anim);
+    }
+
+    }
+
+
+
+
