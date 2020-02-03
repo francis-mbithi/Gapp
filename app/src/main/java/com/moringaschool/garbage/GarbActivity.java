7ca8e20ff1234ae07d0f04343c9283d1fd412de4
francis-mbithi <frankmbithifm@gmail.com>
2020-02-03 10:00:33 +0300

    change views to main activity layout

--- app/src/main/java/com/moringaschool/garbage/GarbActivity.java
+++ app/src/main/java/com/moringaschool/garbage/GarbActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import butterknife.BindView;
+
 public class GarbActivity extends AppCompatActivity {
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView) ListView mListView;
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
+    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs",
+            "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups",
+            "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garb);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(GarbActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
     }
+
+
 }
