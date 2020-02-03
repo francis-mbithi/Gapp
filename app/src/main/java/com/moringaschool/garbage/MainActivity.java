ad83a2da771fa4215fd8ab7695f1eb7437e57cc3
francis-mbithi <frankmbithifm@gmail.com>
2020-02-03 09:54:54 +0300

    bind garb array

--- app/src/main/java/com/moringaschool/garbage/MainActivity.java
+++ app/src/main/java/com/moringaschool/garbage/MainActivity.java
@@ -5,10 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+//        Typeface caviarFont = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams.ttf");
+        mAppNameTextView.setTypeface(caviarFont);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, GarbActivity.class);
+                String location = mLocationEditText.getText().toString();
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
     }
 }
